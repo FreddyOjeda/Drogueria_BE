d2b5a5ca77bad9e885c359205facdeb424ba8ddb
FreddyOjeda <freddy.ojeda@uptc.edu.co>
2023-07-03 17:16:53 -0500

    creacion de los modelos v1

---
+++ drogueria_app/users/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    identify_card = models.IntegerField
+    phone_number = models.IntegerField
+    role = models.CharField(max_length=50)
+    user = models.CharField(max_length=80)
+    password = models.CharField(max_length=100)
+
+class Category(models.Model):
+    name = models.CharField(max_length=80)
+    description = models.CharField(max_length=250)
+
+class Product(models.Model):
+    name = models.CharField(100)
+    description = models.CharField(max_length=250)
+    laboratory = models.CharField(max_length=100)
+    INVIMA = models.CharField(150)
+    batch = models.CharField(150)
+    expiration_date = models.DateField("expiration date")
+    price = models.IntegerField
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+class Regiter(models.Model):
+    stock = models.IntegerField
+    date_entry = models.DateField("date entry")
+    total_value = models.IntegerField
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
