51728d1a049acf227461d2c8ce2bd463a60f6e51
FreddyOjeda <freddy.ojeda@uptc.edu.co>
2023-07-22 12:15:23 -0500

    ya no se

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\USUARIO\OneDrive\Documentos\Semestre 9\Gerencia\pRINMER %0\Drogueria_BE\venv
+set VIRTUAL_ENV=C:\Users\BDG-FOJEDA\Documents\Drogueria_BE\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
