68a77c0591319d0c212cb27b7b48eea5e4daf061
FreddyOjeda <freddy.ojeda@uptc.edu.co>
2023-07-22 12:07:49 -0500

    Despliegue vercel v1

--- drogueria_app/drogueria_app/settings.py
+++ drogueria_app/drogueria_app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^gwm@dcxec#w(n!-kh(#x+u77jrrj1gy$ob62ugvas&&0_ftrt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app','127.0.0.1']
 
 
 
